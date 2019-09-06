784dff7ab0fff5e3014985925056971cc49d906f
pedroheavy <39525797+pedroheavy@users.noreply.github.com>
2019-09-06 00:47:50 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,10 +1,16 @@
 import socket
-HOST = '0.0.0.0'              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-orig = (HOST, PORT)
-udp.bind(orig)
-while True:
-    msg, cliente = udp.recvfrom(1024)
-    print( cliente, msg)
-udp.close()
+
+# set up the socket using local address
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket.bind(("", 9999))
+
+while 1:
+
+    # get the data sent to us
+    data, ip = socket.recvfrom(1024)
+
+    # display
+    print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+
+    # echo back
+    socket.sendto(data, ip)
