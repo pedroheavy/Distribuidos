33c9615410cfe9586f4f07cfd5503b5f697edeb9
pedroheavy <39525797+pedroheavy@users.noreply.github.com>
2019-09-06 01:32:57 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,16 +1,17 @@
 import socket
 
-# set up the socket using local address
+# configura o tipo do socket e define o local
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket.bind(("", 9999))
 
 while 1:
 
-    # get the data sent to us
+    # pega os dados recebidos com buffer
     data, ip = socket.recvfrom(1024)
 
-    # display
-    print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+    # envia mensagem de volta
+    envia_deVolta = socket.sendto(data, ip)
 
-    # echo back
-    socket.sendto(data, ip)
+    print("A mensagem recebida foi: %s"%(data.decode("utf-8")))
+    print("O tamanho dos dados recebidos foi %s" %(len(data)))
+    print("O tamanho dos dados enviados de volta foi de %s"%(int(envia_deVolta)))
